edf5abcdfedb73ad7e38bee9aba610040fdf5245
IT-YASH <pyash7646@gmail.com>
2024-09-10 12:49:00 +0530

    made some changes in it

--- WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -4,14 +4,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WhiteLagoon.Domain.Entities;
 
 namespace WhiteLagoon.Infrastructure.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options) 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
+        }
+        public DbSet<Villa> Villas {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa
+                {
+                    Id = 1,
+                    Name = "Yash Patel",
+                    Descrition = "hello this is yash here",
+                    Occupancy = 4,
+                    Price=200,
+                    Sqft=500
+                },
+                new Villa
+                {
+                    Id = 2,
+                    Name = "Rushabh Gadhi",
+                    Descrition = "hello this is Rushabh Gandhi",
+                    Occupancy = 5,
+                    Price = 300,
+                    Sqft = 600
+                }
+                );
+            // base.OnModelCreating(modelBuilder);
         }
     }
 }
---
+++ WhiteLagoon.Infrastructure/Migrations/20240821095906_seedVillastodb.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedVillastodb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "CreateDate", "Descrition", "Imageurl", "Name", "Occupancy", "Price", "Sqft", "UpdateDate" },
+                values: new object[,]
+                {
+                    { 1, null, "hello this is yash here", null, "Yash Patel", 4, 200.0, 500, null },
+                    { 2, null, "hello this is Rushabh Gandhi", null, "Rushabh Gadhi", 5, 300.0, 600, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
