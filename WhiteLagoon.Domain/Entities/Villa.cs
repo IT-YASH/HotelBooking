40358190d506f1a5ff91669e043d09aea88de9dd
IT-YASH <pyash7646@gmail.com>
2024-09-10 15:51:07 +0530

    made some changes

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,11 @@ namespace WhiteLagoon.Domain.Entities
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Descrition { get; set; }
+        [Display(Name="Price Per Night")]
         public double Price { get; set; }
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
+        [Display(Name = "Image Url")]
         public string? Imageurl { get; set; }
         public DateTime? CreateDate { get; set; }
         public DateTime? UpdateDate { get; set; }
